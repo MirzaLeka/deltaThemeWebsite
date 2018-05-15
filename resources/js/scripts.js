050e67837443f8a108451cbc36ebd5be83c41579
Mirza Leka <mirza_376@hotmail.com>
2018-05-15 23:56:23 +0200

    Replacing old code with ES6 template literals. Grammar fixes

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -7,7 +7,7 @@ let arrayTitle = [
 ];
 let arraySubT = [
     "Questions about Lotus Themes products and services",
-    "All you need to know aobut shoppint at Lotus Themes",
+    "All you need to know about shopping at Lotus Themes",
     "Instructions related to themes installation and usage",
     "Guidelines in case something goes wrong"
 ];
@@ -15,15 +15,15 @@ let arrayPic = [];
 
 let containerContent = '';
 for(let i = 0; i < arrayTitle.length; i++) {
-containerContent += "<div class='col-sm-6 customCSS'>"
-+ "<div class='col-sm-3'>"
-+ "<img src='http://www.clker.com/cliparts/l/u/5/P/D/A/arrow-50x50-md.png' />"
-+ "</div>"
-+ "<div class='col-sm-9'>"
-+  "<h3>" + arrayTitle[i] + "</h3>"
-+ "<p>" + arraySubT[i] + "</p>"
-+ "</div>"
-+ "</div>";
+containerContent += `<div class='col-sm-6 customCSS'>
+<div class='col-sm-3'>
+<img src='http://www.clker.com/cliparts/l/u/5/P/D/A/arrow-50x50-md.png' />
+</div>
+<div class='col-sm-9'>
+<h3>${arrayTitle[i]}</h3>
+<p>${arraySubT[i]}</p>
+</div>
+</div>`;
 }
 
 $("#row").html(containerContent);
